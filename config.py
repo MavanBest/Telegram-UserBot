fd37bc5bb6edd40e08b5ce521bfd6293e3062c2e
MavanBest <n64532235@gmail.com>
2025-05-08 14:34:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_ID = 21004939  # Получите на my.telegram.org
-API_HASH = '05b2b4afbae9aecfd3dfd34893afff6f'  # Получите там же
+API_ID = 'значение'  # Получите на my.telegram.org
+API_HASH = 'значение'  # Получите там же
 PREFIXES = ['.l', '.tlp', '.c', '.cosmo']
